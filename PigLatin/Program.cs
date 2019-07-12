b6a74d788d06bda534c5e6ca2a8b4456da29c8f8
BluEyesWhiteJason <jason.speet@gmail.com>
2019-07-12 14:28:07 -0400

    first commit, vowel start working

---
+++ PigLatin/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace PigLatin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the word to be translated: ");
+
+            string word = Console.ReadLine();
+
+            string lilWord = word.ToLower();
+
+            Console.WriteLine(lilWord);
+
+            string vowels = "aeiou";
+            string pigWord;
+
+            if (vowels.Contains(lilWord[0]))
+            {
+                pigWord = string.Concat(word, "way");
+            }
+            else
+            {
+                pigWord = "ayy";
+            }
+            Console.WriteLine(pigWord);
+        }
+    }
+}
