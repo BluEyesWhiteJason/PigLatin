5098d498e7a7c269b8c3b68141364adbd672ba67
BluEyesWhiteJason <jason.speet@gmail.com>
2019-07-14 13:09:23 -0400

    vowels and consonants working

--- PigLatin/Program.cs
+++ PigLatin/Program.cs
@@ -12,18 +12,31 @@ namespace PigLatin
 
             string lilWord = word.ToLower();
 
-            Console.WriteLine(lilWord);
+            //Console.WriteLine(lilWord);
 
             string vowels = "aeiou";
             string pigWord;
 
+            // If word begins with vowel, add -way
             if (vowels.Contains(lilWord[0]))
             {
                 pigWord = string.Concat(word, "way");
             }
+
+            // If begins with consonant, split at first vowel
             else
             {
-                pigWord = "ayy";
+                pigWord = "hello";
+                for (int i = 1; i < word.Length; i++)
+                {
+                    if (vowels.Contains(lilWord[i]))
+                    {
+                        char c = lilWord[i];
+                        string[] parts = lilWord.Split(lilWord[i]);
+                        pigWord = string.Concat(c, parts[1], parts[0], "ay");
+                        break;
+                    }
+                }
             }
             Console.WriteLine(pigWord);
         }
