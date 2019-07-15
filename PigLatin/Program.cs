c90ee1bd0afb8ed8123e10d1574198d313569a1e
BluEyesWhiteJason <jason.speet@gmail.com>
2019-07-15 18:43:59 -0400

    Added continue loop

--- PigLatin/Program.cs
+++ PigLatin/Program.cs
@@ -6,39 +6,55 @@ namespace PigLatin
     {
         static void Main(string[] args)
         {
-            Console.Write("Enter the word to be translated: ");
-
-            string word = Console.ReadLine();
-
-            string lilWord = word.ToLower();
-
-            //Console.WriteLine(lilWord);
-
-            string vowels = "aeiou";
-            string pigWord;
-
-            // If word begins with vowel, add -way
-            if (vowels.Contains(lilWord[0]))
+            bool cont = true;
+            while (cont == true)
             {
-                pigWord = string.Concat(word, "way");
-            }
+                Console.Write("Enter the word to be translated: ");
 
-            // If begins with consonant, split at first vowel
-            else
-            {
-                pigWord = "hello";
-                for (int i = 1; i < word.Length; i++)
+                string word = Console.ReadLine();
+
+                string lilWord = word.ToLower();
+
+                //Console.WriteLine(lilWord);
+
+                string vowels = "aeiou";
+                string pigWord;
+
+                // If word begins with vowel, add -way
+                if (vowels.Contains(lilWord[0]))
                 {
-                    if (vowels.Contains(lilWord[i]))
+                    pigWord = string.Concat(word, "way");
+                }
+
+                // If begins with consonant, split at first vowel
+                else
+                {
+                    pigWord = lilWord;
+                    for (int i = 1; i < word.Length; i++)
                     {
-                        char c = lilWord[i];
-                        string[] parts = lilWord.Split(lilWord[i]);
-                        pigWord = string.Concat(c, parts[1], parts[0], "ay");
-                        break;
+                        if (vowels.Contains(lilWord[i]))
+                        {
+                            char c = lilWord[i];
+                            string[] parts = lilWord.Split(lilWord[i]);
+                            pigWord = string.Concat(c, parts[1], parts[0], "ay");
+                            break;
+                        }
                     }
                 }
+                Console.WriteLine(pigWord);
+                Console.WriteLine("Continue? y/n : ");
+                string contEnter = Console.ReadLine();
+
+                // Check if they want to continue
+                if (contEnter == "y" || contEnter == "Y")
+                {
+                    cont = true;
+                }
+                else if (contEnter == "n" || contEnter == "N")
+                {
+                    cont = false;
+                }
             }
-            Console.WriteLine(pigWord);
         }
     }
 }
